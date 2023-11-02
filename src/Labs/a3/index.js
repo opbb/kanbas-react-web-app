9af2a60e07ed59bd6882226a24fd83c0eb9baded
Oliver Baer-Benson <opbb83@gmail.com>
2023-11-01 20:10:09 -0400

    Lab 4 Completed

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -4,11 +4,20 @@ import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todo/TodoList";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 function Assignment3() {
+const { todos } = useSelector((state) => state.todosReducer);
  return (
    <div class="p-3">
      <h1>Assignment 3</h1>
+     <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
      <TodoList/>
      <ConditionalOutput/>
      <Styles/>
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,19 +2,23 @@ import {Routes, Route, Navigate} from "react-router";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import Nav from "../Nav";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
   return (
-    <div className="d-flex flex-column">
-      <div><Nav/></div>
-      <div>
-        <Routes>
-              <Route path="/" element={<Navigate to="A3" />} />
-              <Route path="A3" element={<Assignment3 />} />
-              <Route path="A4" element={<Assignment4 />} />
-        </Routes>
+    <Provider store={store}>
+      <div className="d-flex flex-column">
+        <div><Nav/></div>
+        <div>
+          <Routes>
+                <Route path="/" element={<Navigate to="A3" />} />
+                <Route path="A3" element={<Assignment3 />} />
+                <Route path="A4" element={<Assignment4 />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 export default Labs;
