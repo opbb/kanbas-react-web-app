090706c414a01650c298d15b750e4eb462fb004c
Oliver Baer-Benson <opbb83@gmail.com>
2023-11-01 20:13:23 -0400

    Revert "Lab 4 Completed"
    This reverts commit 9af2a60e07ed59bd6882226a24fd83c0eb9baded.

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,23 +2,19 @@ import {Routes, Route, Navigate} from "react-router";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import Nav from "../Nav";
-import store from "./store";
-import { Provider } from "react-redux";
 
 function Labs() {
   return (
-    <Provider store={store}>
-      <div className="d-flex flex-column">
-        <div><Nav/></div>
-        <div>
-          <Routes>
-                <Route path="/" element={<Navigate to="A3" />} />
-                <Route path="A3" element={<Assignment3 />} />
-                <Route path="A4" element={<Assignment4 />} />
-          </Routes>
-        </div>
+    <div className="d-flex flex-column">
+      <div><Nav/></div>
+      <div>
+        <Routes>
+              <Route path="/" element={<Navigate to="A3" />} />
+              <Route path="A3" element={<Assignment3 />} />
+              <Route path="A4" element={<Assignment4 />} />
+        </Routes>
       </div>
-    </Provider>
+    </div>
   );
 }
 export default Labs;
